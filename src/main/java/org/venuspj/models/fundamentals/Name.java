16e6162bf360b2302cc32ead54e3408bfa5b1695
溝口顕一 <k-mizoguchi@den.jagunma.net>
2019-02-18 16:33:35 +0900

    NameメソッドにgetValue()メソッドを追加した

--- src/main/java/org/venuspj/models/fundamentals/Name.java
+++ src/main/java/org/venuspj/models/fundamentals/Name.java
@@ -1,14 +1,33 @@
 package org.venuspj.models.fundamentals;
 
 public class Name {
-	protected String name;
 
-	protected Name(String name) {
-		this.name = name;
+	protected String value;
+
+	/**
+	 * コンストラクター.
+	 * @param value 名称
+	 */
+	protected Name(String value) {
+		this.value = value;
 	}
 
-	public static Name of(String name) {
-		return  new Name(name);
+	/**
+	 * 値を取得する
+	 * @return 値を取得する
+	 */
+	public String getValue() {
+	  return value;
+
+	}
+
+	/**
+	 * Nameインスタンスを取得する
+	 * @param value 名前
+	 * @return valueの設定されたNameオブジェクト
+	 */
+	public static Name of(String value) {
+		return  new Name(value);
 
 	}
 
--- src/test/java/org/venuspj/models/fundamentals/NameTest.java
+++ src/test/java/org/venuspj/models/fundamentals/NameTest.java
@@ -1,12 +1,22 @@
 package org.venuspj.models.fundamentals;
 
+import static org.assertj.core.api.Java6Assertions.*;
 import org.junit.Test;
 
 public class NameTest {
 
 	@Test
-	public void test() {
+	public void of() {
 		Name name = Name.of("dummy");
+        assertThat(name)
+        .isNotNull();
 	}
+
+    @Test
+    public void getValue() {
+        Name name = Name.of("dummy");
+        assertThat(name.getValue())
+        .isEqualTo("dummy");
+    }
 
 }
